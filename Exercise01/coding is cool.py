3ad39c233659935fb8d5de59206d7f3b58e3cd87
farahashfaq <xxfarahashxx@gmail.com>
2024-10-16 14:52:19 +0400

    codingiscool.py
    coding is cool

---
+++ Exercise01/coding is cool.py
@@ -0,0 +1,12 @@
+#Putting the first word into the word1 variable
+word1 = "Coding"
+
+#Putting the second word into the word2 variable
+word2 = "is"
+
+#Putting the third word into the word3 variable
+word3 = "cool"
+
+#Using string concentration, the variables are joined with gaps in between.
+#and printing the phrase "Coding is cool" to the counsole
+print(word1 + " " + word2 + " " + word3)
