03bcf4558fb258ee2af7745124a3d7b4e5916030
farahashfaq <xxfarahashxx@gmail.com>
2024-10-22 17:31:40 +0400

    simplesup.py
    simple sum.py

---
+++ Exercise02/simple sum.py
@@ -0,0 +1,18 @@
+#Steps:
+#1 Create a variable and set its initial value to the whole number '8'.
+#2 Create a second variable and set its initial value to the whole number '10'.
+#3 Assign the total of the first two integers to a third variable.
+#4 Output the total sum value to the console.
+
+#simple sums
+#adding a number to the first variable
+lady = (int(8))
+
+#adding a number to the second variable
+bug = (int(10))
+
+#adding both the first and second variable to the third variable
+jose = (lady + bug)
+
+#print the variable moraino
+print(jose)
