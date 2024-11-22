0562f67cfacbf56658826f5c353c3d8e0886a7c9
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:44:49 +0400

    Function-Favourite book.py
    Function-Favourite book.py

---
+++ z_Bonus/Function-Favourite Book.py
@@ -0,0 +1,10 @@
+#Create the favorite_book() function, which takes title as its only parameter.
+#The message that the function prints should read, "Alice in Wonderland is one of my favorite books."
+#Include the book title as a parameter in the function call when you call it.
+
+#Outlining the function
+def favourite_book_name(title):
+    print(f" {title} is one of my favorite books")
+
+#Using the book title as a parameter when calling the function
+favourite_book_name("Alice in Wonderland")
