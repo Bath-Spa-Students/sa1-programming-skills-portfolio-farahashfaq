446201b8e5f2e8cc53c775df3fd91806eda310bf
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:43:07 +0400

    Control Structure.py
    Control Structure.py

---
+++ z_Bonus/Control Structure.py
@@ -0,0 +1,16 @@
+#Pass Test:
+
+# Assign the color of the alien.
+alien_color = 'Red'
+
+# Verify whether the alien is red in color.
+if alien_color == 'Red':
+    print("The player just received five points.")
+
+#Fail Test:
+
+# Assign the color of the alien.
+alien_color = 'Red'
+# Verify whether the alien is red in color.
+if alien_color == 'Green':
+    print("The player just received five points.")
