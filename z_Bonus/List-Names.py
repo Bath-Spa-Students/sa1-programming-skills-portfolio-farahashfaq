7b117790768fbbbc5cdada5f49f194f547ad8065
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:45:33 +0400

    List-Names.py
    List-Names.py

---
+++ z_Bonus/List-Names.py
@@ -0,0 +1,8 @@
+# Maintain a list of your friends' names.
+names = ["Matin", "Tasneem", "Faris", "Shahwaiz"]
+
+# Print the names of each individual one at a time.
+print(names[0])  #First name
+print(names[1])  #Second name
+print(names[2])  #Third name
+print(names[3])  #Fourth name
