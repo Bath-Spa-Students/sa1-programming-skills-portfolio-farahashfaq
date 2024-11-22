1f50d3aa442c1b5ef4936b21bd3e46bae49f3580
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:43:34 +0400

    Dictionaries.py
    Dictionaries.py

---
+++ z_Bonus/Dictionaries.py
@@ -0,0 +1,17 @@
+#Store information about someone you know in a dictionary.Keep their first name on file.
+#final name, age, and city of residence.  
+#You should have such keys as city, age, last name, and first name. Print every individual piece of information stored in your dictionary.
+
+#Developing the dictionary
+Person_information = {
+    "First name": "Farah",
+    "Last name": "Ashfaq",
+    "Age": 18,
+    "City" : "Fujairah, Masafi"
+}
+
+#Printing every item of data
+print(f"First Name: {Person_information['First name']}")
+print(f"Last name: {Person_information['Last name']}")
+print(f"Age: {Person_information['Age']}")
+print(f"City: {Person_information['City']}")
