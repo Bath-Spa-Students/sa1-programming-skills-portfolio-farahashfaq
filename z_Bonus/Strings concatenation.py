4ffe1f8236ff9bf1a279f67e62084456e6801b75
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:46:03 +0400

    Strings concatenation.py
    Strings concatenation.py

---
+++ z_Bonus/Strings concatenation.py
@@ -0,0 +1,8 @@
+#Three strings should be defined in separate variables.
+string1 = "Hello"
+string2 = "Miss"
+string3 = "Rafiya"
+
+# Merge the strings and output the outcome.
+Result = string1 + " " + string2 + " " + string3
+print(Result)
