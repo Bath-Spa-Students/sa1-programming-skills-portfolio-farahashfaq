cb2ba863b9131762e31aab2a9dbbcaf7e8cb547f
farahashfaq <xxfarahashxx@gmail.com>
2024-11-22 22:35:23 +0400

    Simple_search.py

--- Exercise08/Simple_search.py
+++ Exercise08/Simple_search.py
@@ -1,23 +1,23 @@
-##make a list of strings which contains names
+#Make a list of strings which contains names
 Names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
 #In the above string list, write the string you wish to search for.
 Search_name = input("Enter name:").strip()
 
-# Flag to monitor if the name appears in the search
+#Flag to monitor if the name appears in the search
 found = False
 
-# Check if the user's input matches each name in the list by looping over them.
+#Check if the user's input matches each name in the list by looping over them.
 for name in Names:
-    # To make sure the search is case-insensitive, convert both names to lowercase.
+    #To make sure the search is case-insensitive, convert both names to lowercase.
 
     if name.lower() == Search_name.lower():
-        # Set 'found' to True and notify the user if a match is discovered.
+        #Set 'found' to True and notify the user if a match is discovered.
         found = True
         print(f"perfect'{Search_name}' is present in the list.")
 
-        break  # Since we discovered the name, end the loop early.
-# Inform the user if the loop ends and the name was not found.
+        break  #Since we discovered the name, end the loop early.
+#Inform the user if the loop ends and the name was not found.
 
 if not found:
     print(f"sadly, '{Search_name}' is not in the list.")
